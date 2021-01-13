515e525ad299d9e02c655d3d3582957b7e29d646
netzon-eladio <eladio.sareno@netzontech.com>
2021-01-13 08:49:39 +0800

    chore(wip): Add models package

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from typing import Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from models.models import Item
+
 
 app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
 
 
 @app.get("/")
